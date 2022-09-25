3263400744b349571a711005f1818753aba4bb1f
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-09-25 15:44:05 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryRef = document.querySelector(".gallery");
+const markupOfGallery = createMarkupOfGallery(galleryItems);
+
+galleryRef.insertAdjacentHTML("afterbegin", markupOfGallery);
+
+function createMarkupOfGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item"><a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"        
+        alt="${description}"
+      />
+    </a></li>`
+    ).join("");
+};
+
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+    })
