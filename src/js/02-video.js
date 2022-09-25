2a6dd8a1458d959d9e5825763e51e8fcb9abb7df
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-09-25 17:59:55 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+
+    player.on('timeupdate', throttle(function(data) {
+        console.log(data.seconds);
+        localStorage.setItem("videoplayer-current-time", data.seconds);
+    }, 1000));
+
+    const savedTime = Number(localStorage.getItem("videoplayer-current-time"))
+;
+    player.setCurrentTime(savedTime).then(function (seconds) {
+        console.log(seconds);
+});
